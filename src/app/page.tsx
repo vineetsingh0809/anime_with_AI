afba96064cbbd3a35c1cbfc757e386885b0aae1c
vineetsingh0809 <ankitchilma@gmail.com>
2023-08-22 14:59:38 +0530

    Added login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/HeaderComponent/Header";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 export default function Home() {
   return (
     <div className="relative">
-      <Header/>
-      <HomePage />
+      <Header />
+      {/* <HomePage /> */}
+      <LoginPage />
     </div>
   );
 }
